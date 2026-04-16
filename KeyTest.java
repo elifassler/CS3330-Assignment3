a1680ea114a590f6591f9435d9a7682ebea5c358
Jwhalen23 <120579598+Jwhalen23@users.noreply.github.com>
2026-04-16 16:25:04 -0500

    added parameterized test to key tester

--- KeyTest.java
+++ KeyTest.java
@@ -3,44 +3,41 @@ package com.example.haunted.model;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KeyTester {
 
-	public KeyTester() {
-		@Test
-		void constructorSetsKeyProperties() { 
-			Key key = new key("Trumans Key", "Opens all buildings on campus");
-			assertEquals("Trumans Key", key.getName());
-			assertEquals("Opens all buildings on campus", key.getDescription());
-			
-		}
-			
-		@Test 
-		void keyIsNotEquippable() {
-			Key key = new Key("Pickelmans Key", "Opens Pickelmans");
-			assertFalse(key instanceof Equippable,"Cannot equip this item.");
-			
-		}
-		
-		@Test 
-		void toStringReturnsKeyName() {
-			Key key = new Key("Dr.Ekin's Key", "Opens Tucker Hall");
-			assertEquals("Dr. Ekin's Key", key.toString());
-		}
-		void SetDefaultNameIfNull() {
-			// Sets Default if Name is Null
-			Key key = new Key(null, "Unidentified key")
-			assertEquals("Unknown Key", key.getName());
-		}
-		
-		void setDefaultDescription() {
-			Key key = new Key("Unknown Key", null);
-			assertEquals("Unidentified Key", key.getDescription());
-			
-			
-		}
-	}
+    @Test
+    void constructorSetsKeyProperties() { 
+        Key key = new Key("Trumans Key", "Opens all buildings on campus");
+        assertEquals("Trumans Key", key.getName());
+        assertEquals("Opens all buildings on campus", key.getDescription());
+    }
+        
+    @Test //Checks that key cant be equipped 
+    void keyIsNotEquippable() {
+        Key key = new Key("Pickelmans Key", "Opens Pickelmans");
+        assertFalse(key instanceof Equippable);
+    }
+    
+    @Test 
+    void toStringReturnsKeyName() {
+        Key key = new Key("Dr. Ekin's Key", "Opens Tucker Hall");
+        assertEquals("Dr. Ekin's Key", key.toString());
+    }
 
+    @ParameterizedTest // Changes all null values to strings 
+    @ValueSource(strings = {"Unknown Key"})
+    // sets default name 
+    void SetDefaultNameIfNull(String expected) {
+        Key key = new Key(null, "Unidentified key"); 
+        assertEquals(expected, key.getName());
+    }
+
+    @Test
+    void setDefaultDescriptionIfNull() {
+    	// Sets Default description 
+        Key key = new Key("Unknown Key", null);
+        assertEquals("Unidentified Key", key.getDescription());
+    }
 }
