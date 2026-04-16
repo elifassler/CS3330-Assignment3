a39c488e1f7136b0130c9d8ace7600352951797e
Jwhalen23 <120579598+Jwhalen23@users.noreply.github.com>
2026-04-16 17:26:04 -0500

    Refactor ItemTest for clarity and consistency

--- ItemTest.java
+++ ItemTest.java
@@ -1,31 +1,36 @@
 package com.example.haunted.model;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*; // Required for assertEquals to work
 
 public class ItemTester {
 	
 	@Test
-	void constructorShouldSetFeildsBySubclass() {
-	Armor item = new Armor("Chestplate", "Diamond", 8); 	
-	assertEquals("Diamond Chestplate", item.getName());
-	assertEquals("Enchanted", item.getDescription());
-	
+	void constructorShouldSetFieldsBySubclass() {
+		// confirms subclass sets name and description 
+		Armor item = new Armor("Chestplate", "Diamond", 8); 	
+		assertEquals("Diamond Chestplate", item.getName());
+		assertEquals("Enchanted", item.getDescription());
 	}
+
 	@Test 
 	void ConstructorAssignsDefaultNameIfNull() {
-	//Sets default name if the name is null
-	Weapon item = new Weapon (null, "Strange Sword", 15);
-	assertEquals("Unknown Weapon", item.getName());
+		// sets a default name for null values 
+		Weapon item = new Weapon(null, "Strange Sword", 15);
+		assertEquals("Unknown Weapon", item.getName());
 	}
 	
 	@Test
 	void ConstructorSetsDefaultIfDescriptionNull() {
-		//Sets Description to Unidentified if null 
-	Key item = new Key("Key", null,);
-	assertEquals("Unidentified Key", item.getDescription());
+		// sets Description if null 
+		Key item = new Key("Key", null);
+		assertEquals("Unidentified Key", item.getDescription());
 	}
 	
 	@Test 
 	void toStringReturnsItemName() {
-	Potion item = new Potion("Light Healing Potion", "A lesser healing potion +10hp", 10);
-	assertEquals("Light Healing Potion", item.toString());
+		//verifies that toString returns name 
+		Potion item = new Potion("Light Healing Potion", "A lesser healing potion +10hp", 10);
+		assertEquals("Light Healing Potion", item.toString());
+	}
+}
