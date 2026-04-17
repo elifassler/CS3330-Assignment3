e48f1e72f423807e1ffdc1c345b30be4878fe90b
matteottij-web <matteottij@gmail.com>
2026-04-16 20:37:35 -0500

    Update QuestItemTest.java

--- QuestItemTest.java
+++ QuestItemTest.java
@@ -1,5 +1,5 @@
 package com.example.haunted.model;
-
+// imports
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 
 
 public class QuestItemTest {
-
+    // creating quest item before test
     private QuestItem questItem;
 
     @BeforeEach
     public void setup() {
         questItem = new QuestItem("My Watch", "A Timex");
     }
-
+    // testing for storing name and description
     @Test
     public void shouldStoreName() {
         assertEquals("My Watch", questItem.getName());
     }
-
+    
     @Test
     public void shouldStoreDescription() {
         assertEquals("A Timex", questItem.getDescription());
