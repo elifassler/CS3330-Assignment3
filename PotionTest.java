635092285602982cb1676dff6e6527dc8121b5cb
matteottij-web <matteottij@gmail.com>
2026-04-16 19:31:49 -0500

    Update PotionTest.java

--- PotionTest.java
+++ PotionTest.java
@@ -1,5 +1,6 @@
 package com.example.haunted.model;
 
+// imports
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,13 +12,13 @@ public class PotionTest {
 
     private Player player;
     private Potion potion;
-
+    // have to make a new player and potion for the test
     @BeforeEach
     public void setup() {
         player = new Player("Jack", 100, 10, 5, new Inventory(5));
         potion = new Potion("Coffee Potion", "Restores energy", 25);
     }
-
+    
     @Test
     public void shouldStoreHealingAmount() {
         assertEquals(25, potion.getHealingAmount()); //
@@ -48,7 +49,7 @@ public class PotionTest {
         player.takeDamage(50);
 
         testPotion.use(player);
-
+        // if statement for parameterized test
         if (50 + healingAmount > 100) {
             assertEquals(100, player.getHealth());
         } else {
